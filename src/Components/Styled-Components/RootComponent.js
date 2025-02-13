0f069ec9b3c3dba1670cec7b3d2884bf463ac933
rashaaad19 <egyoussef_2000@hotmail.com>
2025-02-13 20:00:57 +0200

    style: added new responsive SideBar component

--- src/Components/Styled-Components/RootComponent.js
+++ src/Components/Styled-Components/RootComponent.js
@@ -5,4 +5,13 @@ export const RootContainer=styled.div`
 display:flex;
 height:100%;
 align-items:flex-start;
+
+
+@media(max-width:768px){
+    align-items: flex-start;
+    flex-direction: column-reverse;
+    align-items-flex-start;
+}
 `
+
+
--- src/Components/Styled-Components/SideBarComponent.js
+++ src/Components/Styled-Components/SideBarComponent.js
@@ -1,11 +1,38 @@
 import styled from "styled-components";
 
-export const SideBarContainer = styled.div`
-width:20%;
+export const SideBarContainer = styled.nav`
+box-sizing:border-box;
+width: ${(props) => (props.isOpen ? '280px' : '60px')};
 background-color:#F6F6F6;
 height:100vh;
 display:flex;
 flex-direction:column;
+padding: ${(props) => (props.isOpen ? '5px 1em' : '5px')};
+border-right:1px solid #E5E5E5;
+position:sticky;
+top:0;
+align-self:flex-start; 
+transition:300ms ease-in-out;
+overflow:hidden;
+text-wrap:nowrap;
+
+
+@media(max-width:768px){
+width:100%;
+padding:5px;
+height:auto
+}
+`
+export const NavListContainer = styled.ul`
+list-style:none;
+@media(max-width:768px){
+display:flex;
+justify-content:space-evenly;
+li:first-child{
+display:none}
+
+}
+
 `
 export const SideBarHeader = styled.div`
 padding-top:20px;
@@ -16,8 +43,8 @@ text-align:center;
 display:flex;
 align-items:center;
  gap:10px;
- justify-content:center;
- font-family:math;
+ justify-content:flex-end;
+
 
 `
 export const SideBarHeaderIcon = styled.div`
@@ -25,30 +52,58 @@ export const SideBarHeaderIcon = styled.div`
   align-items: center; /* Vertically center the SVG */
   justify-content: center; /* Horizontally center the SVG */
 
+
+
 `;
 export const SideBarsubHeader = styled.p`
 
 font-size:10px;
 color:#545454;
 text-transform:uppercase;
-padding-left:36px;
-padding-top:10px
+padding:.85em;
 `
 export const SideBarItem = styled.p`
 display:flex;
-gap:5px;
+gap: ${(props) => (props.isOpen ? '5px' : '20px')};
+text-decoration:none;
 align-items:center;
 font-size:16px;
 font-weight:bold;
 color:#777777;
-margin-left:38px;
-margin-top:15px;
 cursor:pointer;
 width:fit-content;
+margin:.85em;
+  @media(max-width:768px){
+span{
+display:none;
+}
+&
+}
 
-margin-top: ${(props) => (props.$lastItem ? 'auto' : 'none')};
-margin-bottom: ${(props) => (props.$lastItem ? '40px' : 'none')};
+
+// margin-top: ${(props) => (props.lastItem ? 'auto' : '0')};
+// margin-bottom: ${(props) => (props.lastItem ? '40px' : '0')};
 `
 
-// margin-top: auto;
-// margin-bottom: 25px;
+export const ToggleButton = styled.button`
+margin-left:auto;
+padding:1em;
+border:none;
+border-radius:.5em;
+background:none;
+cursor:pointer;
+svg{
+transform:rotate(${(props) => (props.isOpen ? '0deg' : '180deg')});
+transition:transform 150ms ease;
+}
+&:hover{
+  background-color:#E5E5E5;
+}
+
+
+@media(max-width:768px){
+display:none;
+}
+
+
+`
