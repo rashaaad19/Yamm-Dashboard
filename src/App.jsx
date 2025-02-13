4293d4791376f94e545409a93bcdbbeb3c1e156a
rashaaad19 <egyoussef_2000@hotmail.com>
2025-02-13 05:28:03 +0200

    chore: - Added db.json file - tested the server in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import DataTable from "./Components/DataTable";
 import Root from "./Pages/Root";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/orders");
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
   let router = createBrowserRouter([
     {
       path: "/",
       Component: Root,
-      children:[
+      children: [
         {
-          path:'',
-          Component:DataTable
+          path: "",
+          Component: DataTable,
         },
-        
-      ]
+      ],
     },
   ]);
   return (
